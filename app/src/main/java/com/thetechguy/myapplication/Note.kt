385271886791e5367267c017e82d7ea26055c086
JatinVashisht1 <jatin8085vashisht@gmail.com>
2021-07-27 13:59:39 +0530

    After shrinking the app and making it secure

--- app/src/main/java/com/thetechguy/myapplication/Note.kt
+++ app/src/main/java/com/thetechguy/myapplication/Note.kt
@@ -2,11 +2,13 @@ package com.thetechguy.myapplication
 
 import android.os.Parcel
 import android.os.Parcelable
+import androidx.annotation.Keep
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
+@Keep
 @Parcelize
 @Entity(tableName = "note_table")
 data class Note(
